725e2f177cdf0edb17dd406f9c2ed4a24c2dca68
hosmi <koike.engineer@gmail.com>
2023-02-24 22:01:28 +0900

    refactor:🚨 resolve warning on useEffect

--- app/components/population/PopulationChart.tsx
+++ app/components/population/PopulationChart.tsx
@@ -78,6 +78,7 @@ export default function PopulationChart() {
     ;(async () => {
       await fetchPopulationComposition(prefectureCodeList)
     })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [prefectureCodeList])
 
   return (
